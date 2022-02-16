9b546204f8809ce025eb7157fb95abff79b86584
Adam Baldwin <abaldwinva98@gmail.com>
2022-02-15 23:16:21 -0500

    First commit

--- src/main/java/com/ultimate/fpl/FplApplication.java
+++ src/main/java/com/ultimate/fpl/FplApplication.java
@@ -2,8 +2,9 @@ package com.ultimate.fpl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class FplApplication {
 
 	public static void main(String[] args) {
