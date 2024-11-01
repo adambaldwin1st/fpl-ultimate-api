1fe9e52fecf72048ccd91738806f1d2a73378089
adambaldwin <abaldwinva98@gmail.com>
2024-11-01 18:37:06 -0400

    Adding basic controller

--- src/main/java/com/fpl/ultimate/controllers/HelloController.java
+++ src/main/java/com/fpl/ultimate/controllers/HelloController.java
@@ -1,7 +1,9 @@
 package com.fpl.ultimate.controllers;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +13,10 @@ public class HelloController {
     @GetMapping
     public String home() {
         return "Hello, this is the base endpoint";
+    }
+
+    @GetMapping(path = "/health", produces = "plain/text")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("FPL Ultimate API is up and running!");
     }
 }
