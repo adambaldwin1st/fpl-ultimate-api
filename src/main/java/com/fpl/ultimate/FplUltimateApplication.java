543b94d10d328406847ffa757265989c751a99d0
adambaldwin <abaldwinva98@gmail.com>
2024-11-10 16:29:41 -0500

    Adding basic controller

--- src/main/java/com/fpl/ultimate/FplUltimateApplication.java
+++ src/main/java/com/fpl/ultimate/FplUltimateApplication.java
@@ -1,8 +1,5 @@
 package com.fpl.ultimate;
 
-import io.github.cdimascio.dotenv.Dotenv;
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -11,17 +8,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories
 public class FplUltimateApplication {
 
-	@Value("${env.path}")
-	private String envPath;
-
 	public static void main(String[] args) {
 		SpringApplication.run(FplUltimateApplication.class, args);
-	}
-
-	@PostConstruct
-	public void init() {
-		System.out.println("Loading .env file from: " + envPath);
-		Dotenv.configure().directory(envPath).filename(".env").load();
 	}
 
 }
